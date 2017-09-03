1a5b38e303758a31c52092ac00fa0255a5dd470a
MarKvoff <Data@sjtu.edu.cn>
2017-09-03 20:28:52 +0800

    solutions for course 1

--- course1/test1.py
+++ course1/test1.py
@@ -1,1 +1,13 @@
-print 'hello'
+import numpy as np
+
+A = np.array([[12,3,4,5],
+             [34,5,2,1],
+             [34,56,4,8]])
+
+print A
+print ' '
+
+cal = A.sum(axis=0)
+print cal
+
+print 100*A/cal
